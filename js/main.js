2fbee72e2b69ce836304fb087fefbe6eb7c19960
Vlad <vmansurov@icloud.com>
2020-10-21 20:48:02 +0300

    Выполнил сложное задание 5 урока

--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 'use strict';
+let arr = ['12', '27', '755', '104', '47', '28', '377'];
 
-function check(str) {
-  if (typeof str !== 'string') {
-    alert('аргумент не строка');
-    return;
+arr.forEach((item) => {
+  if (item.startsWith('2') || item.startsWith('4')) {
+    console.log(item);
   }
+});
 
-  str = str.trim(); 
-  return str.length > 30 ? str.slice(0, 30) + '...' : str;
-}
 
-let string = prompt('Введите небольшой кусок текста');
-console.log(check(string));
+
+// Решение взял с learn.javascript.ru
+let n = 100;
+
+nextPrime:
+  for (let i = 2; i <= n; i++) { // Для всех i...
+
+    for (let j = 2; j < i; j++) { // проверить, делится ли число..
+      if (i % j === 0) {
+        continue nextPrime;
+      } // не подходит, берём следующее
+    }
+
+    console.log( i + ' - Делители этого числа: 1 и ' + i); // простое число
+  }
